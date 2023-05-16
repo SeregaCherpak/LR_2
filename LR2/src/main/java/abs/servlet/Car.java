2aa63d20e62847f2ff90af4e6b5a7dd24475d752
Cherpak <125959803+SeregaCherpak@users.noreply.github.com>
2023-05-16 17:10:30 +0600

    Add files via upload

---
+++ LR2/src/main/java/abs/servlet/Car.java
@@ -0,0 +1,17 @@
+package abs.servlet;
+
+public class Car {
+    private String mark;
+    private String model;
+    private String price;
+    private String year;
+    private String engine_capacity;
+
+    public Car(String mark, String model, String price, String year, String engine_capacity) {
+        this.mark = mark;
+        this.model = model;
+        this.price = price;
+        this.year = year;
+        this.engine_capacity = engine_capacity;
+    }
+}
